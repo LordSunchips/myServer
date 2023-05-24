28b74f209b12c4cde863f560f36ab10a185e77f8
Sanjay Manoj <sanjaymanoj04@gmail.com>
2023-05-24 13:26:21 -0500

    Initial Commit

---
+++ myServer/myServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  myServerApp.swift
+//  myServer
+//
+//  Created by Sanjay Manoj on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct myServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
